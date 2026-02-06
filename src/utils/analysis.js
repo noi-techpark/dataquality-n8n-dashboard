2c2cd0f5942171fd930cd3423440aa99b21dd467
kaiozwald <a.abuhassan@noi.bz.it>
2026-02-06 12:34:00 +0100

    fixed reuse licence files

--- src/utils/analysis.js
+++ src/utils/analysis.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 /**
  * Utility for progressive data analysis.
  * Processes data in chunks to keep memory usage low.
